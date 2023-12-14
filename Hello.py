c999718fdf512083c5b4e1d22e49ecda8502e999
lorae21 <144047749+lorae21@users.noreply.github.com>
2023-12-14 11:01:58 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -15,8 +15,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
+
 import joblib
 import streamlit as st
 import pandas as pd
